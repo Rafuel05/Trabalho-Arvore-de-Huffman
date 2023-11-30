73b8151127eaab6e8c06ea18550483f442fea535
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2023-11-30 14:20:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void main () {
     int colunas;
     CodigoBinario **tabela_codigos;
     setlocale(LC_ALL, "portuguese");
-//----------------------------------------------------------------Função Parte 1: Tabela de Frequência
+//----------------------------------------------------------------Parte 1: Tabela de Frequência
     inicializa_tabela_com_zero(tabela_frequencia);
     preenche_tab_frequencia(arquivo, tabela_frequencia);
     fclose(arquivo);
@@ -27,19 +27,18 @@ void main () {
 //----------------------------------------------------------------Parte 2: Lista Encadeada Ordenada
     criar_lista(&lista);
     preencher_lista(tabela_frequencia, &lista);
-//----------------------------------------------------------------ParteParte 3: Montar a Árvore de Huffman
+//----------------------------------------------------------------Parte 3: Montar a Árvore de Huffman
     arvore = montar_arvore(&lista);
-    printf("\nÁrvore de Huffman:\n");
 //----------------------------------------------------------------Parte 4:Montar o Dicionário
     colunas = altura_arvore(arvore) + 1;
     tabela_codigos = aloca_matriz_codigo(TAM, colunas);
     gerar_tabela_codigos(arvore, tabela_codigos, "", colunas);
     imprime_tabela_codigos(tabela_codigos);
-    // Comprimir
+//---------------------------------------------------------------------------------------- Comprimir
     printf("\nComprimindo...\n");
     comprimir_arquivo("teste.txt", "arquivo_comprimido.bin", tabela_codigos);
     printf("Arquivo comprimido com sucesso.\n");
-    //substitui de acordo com a tabela ascii
+//---------------------------------------------------------------------------------------- substitui de acordo com a tabela ascii
     processarArquivoBinario("arquivo_comprimido.bin", "arquivo_comprimido_1.txt");
 
 
